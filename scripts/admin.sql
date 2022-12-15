32d357d0061744db958b760794db691d3719c7b4
Bikash <bikashprakashdash@gmail.com>
2022-12-15 11:14:32 +0530

    first commit

---
+++ scripts/admin.sql
@@ -0,0 +1,6 @@
+CREATE USER IF NOT EXISTS root@localhost IDENTIFIED BY 'root';
+SET PASSWORD FOR root@localhost = PASSWORD('root');
+GRANT ALL ON *.* TO root@localhost WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS root@'%' IDENTIFIED BY 'root';
+SET PASSWORD FOR root@'%' = PASSWORD('root');
+GRANT ALL ON *.* TO root@'%' WITH GRANT OPTION;
